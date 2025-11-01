d0ac1e2cc1ee45ba458a0e61ed730526331911d9
brendaklopez <103277113+brendaklopez@users.noreply.github.com>
2025-11-01 00:41:28 -0300

    Delete backend directory

--- backend/repository/model/MenuCategory.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const MenuCategorySchema = new mongoose.Schema({
-  nombre: { type: String, required: true, unique: true, trim: true },
-  orden: { type: Number, default: 0 },
-}, { timestamps: true });
-
-export const MenuCategory = mongoose.model('MenuCategory', MenuCategorySchema);
